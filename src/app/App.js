37642f8ddbf47ee1a8caab54058cbbdf4dce4b11
plotrekhq <plotrekhqpl@gmail.com>
2021-01-28 14:24:03 +0100

    initial commit

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+          <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/app/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      
+    </>
+  );
+};
